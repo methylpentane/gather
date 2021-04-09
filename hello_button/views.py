562429f123923ed5fa8cbd417e34b6f2cdbe68bc
methylpentane <v47r2bu9@gmail.com>
2021-04-10 01:11:29 +0900

    fix message

--- hello_button/views.py
+++ hello_button/views.py
@@ -94,7 +94,7 @@ def index(request):
     member = request.GET.get('member', default=None)
     if member:
         login()
-        post_msg(user['id'], f'(bot) this is test message. member is {member}', settings.DEBUG)
+        post_msg(user['id'], f'(bot) {member} said "hello virtual esslab!"', settings.DEBUG)
 
     object_list = LabMembers.objects.all()
     context = {'debug': settings.DEBUG, 'object_list': object_list, 'member': member}
