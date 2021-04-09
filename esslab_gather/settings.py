602d2076aac5c5da893ee662659b90125d6f2eef
methylpentane <v47r2bu9@gmail.com>
2021-04-09 23:52:08 +0900

    database ga genninnnkana

--- esslab_gather/settings.py
+++ esslab_gather/settings.py
@@ -92,6 +92,11 @@ DATABASES = {
     }
 }
 
+if not DEBUG:
+    import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
